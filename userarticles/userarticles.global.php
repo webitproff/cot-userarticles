2e9b162adce296a00f4c8562147069fa18663950
sodium carbonate <webitproff@gmail.com>
2025-03-19 03:08:39 +0300

    Add files via upload

---
+++ userarticles/userarticles.global.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * [BEGIN_COT_EXT]
+ * Hooks=global
+ * [END_COT_EXT]
+ */
+
+
+defined('COT_CODE') or die('Wrong URL.');
+
+require_once cot_langfile('userarticles', 'plug');
+
+// Здесь можно добавить глобальные хуки, если нужно
+
+
